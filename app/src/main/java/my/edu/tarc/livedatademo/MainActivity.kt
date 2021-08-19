625dc6bde67075525b245b7c96e8e6bf8f57089d
rongshen0331 <rongshen0331@gmail.com>
2021-08-19 16:26:31 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/livedatademo/MainActivity.kt
@@ -0,0 +1,50 @@
+package my.edu.tarc.livedatademo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import my.edu.tarc.livedatademo.databinding.ActivityMainBinding
+import my.edu.tarc.livedatademo.models.AccountModel
+
+class MainActivity : AppCompatActivity() {
+    private val myAccout = AccountModel("A1234566","John", 500.00)
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding =  DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myAccount = myAccout
+        binding.lifecycleOwner = this
+
+       // display()
+        //myAccout.balance.observe(this, Observer{newBalance -> binding.tvBalance.text = newBalance.toString() })
+
+
+        binding.btnDeposit.setOnClickListener(){
+            val amount:Double  = binding.tfAmount.text.toString().toDouble()
+
+            myAccout.deposit(amount)
+           // binding.tvBalance.text = myAccout.balance.toString()
+
+        }
+
+        binding.btnWitdraw.setOnClickListener(){
+            val amount:Double  = binding.tfAmount.text.toString().toDouble()
+            myAccout.witdraw(amount)
+
+            //binding.tvBalance.text = myAccout.balance.toString()
+
+        }
+    }
+//
+  //  fun display(){
+//
+   //     binding.tvAccNo.text = myAccout.accNo
+  //      binding.tvName.text = myAccout.owner
+    //    binding.tvBalance.text = myAccout.balance.value.toString()
+
+   // }
+}
---
+++ app/src/main/java/my/edu/tarc/livedatademo/models/AccountModel.kt
@@ -0,0 +1,30 @@
+package my.edu.tarc.livedatademo.models
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class AccountModel(accNo:String, owner:String, balance:Double) :ViewModel(){
+    var accNo : String = ""
+    var owner : String = ""
+    var _balance = MutableLiveData<Double>()
+    val balance: LiveData<Double>
+                get() = _balance
+
+    init {
+        this.accNo = accNo
+        this.owner = owner
+        this._balance.value = balance
+    }
+
+
+
+    fun deposit(amount: Double) {
+        _balance.value = (_balance.value)?.plus(amount)
+    }
+
+    fun witdraw(amount:Double){
+        _balance.value = (_balance.value)?.minus(amount)
+    }
+
+}
